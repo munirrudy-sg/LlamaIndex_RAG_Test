beaecef0c9c5260d1d7c5adbe78fa6b5d2fccdc3
harry.maringant <harry.maringan@banksinarmas.com>
2024-07-26 11:10:07 +0700

    edit path

--- ui/src/agent.py
+++ ui/src/agent.py
@@ -63,7 +63,7 @@ class SQLAgent:
 
     def generate_response(self, query: str):
         # Write prompt to guide the LLM to generate response
-        system_prefix = """You are an agent designed to interact with a SQL database.
+        system_prefix = """You are an agent designed to interact with a SQL database, answer in 'Bahasa Indonesia'.
         Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
         Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
         You can order the results by a relevant column to return the most interesting examples in the database.
