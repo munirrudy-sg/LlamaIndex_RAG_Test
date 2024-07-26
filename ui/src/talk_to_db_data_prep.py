1f7ae130717e4c7aa87606da78437ab0699175d3
harry.maringant <harry.maringan@banksinarmas.com>
2024-07-26 11:48:41 +0700

    update path

--- ui/src/talk_to_db_data_prep.py
+++ ui/src/talk_to_db_data_prep.py
@@ -12,6 +12,9 @@ from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 
 gemini_api_key = st.secrets["gemini_api_key"]
 
+dir = path.Path(__file__).abspath()
+sys.path.append(dir.parent.parent)
+
 # load model
 path_to_query = './examples.json'
 
